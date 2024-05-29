8bcf5f5fd4aa2772657878bfd82a8e551a7afb2e
Marika Abe <59960189+Marikita1007@users.noreply.github.com>
2024-05-29 14:52:18 +0200

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,83 @@
+import "./styles.css";
+
+const skills = [
+  {
+    name: "Javascript",
+    emoji: "💪",
+    backgroundColor: "lightsteelblue",
+  },
+  {
+    name: "HTML+CSS",
+    emoji: "💪",
+    backgroundColor: "lightblue",
+  },
+  {
+    name: "Web Design",
+    emoji: "✌️",
+    backgroundColor: "yellow",
+  },
+  {
+    name: "Git and GitHub",
+    emoji: "👍",
+    backgroundColor: "green",
+  },
+  {
+    name: "React",
+    emoji: "✌️",
+    backgroundColor: "orange",
+  },
+  {
+    name: "Symfony",
+    emoji: "👍",
+    backgroundColor: "pink",
+  },
+];
+
+export default function App() {
+  return (
+    <div className="card">
+      <Avatar />
+      <Data skills={skills} />
+    </div>
+  );
+}
+
+function Avatar() {
+  return (
+    <>
+      <img src="/images/avatar.jpg" alt="Avatar" className="avatar" />
+    </>
+  );
+}
+
+function Data({ skills }) {
+  return (
+    <main className="data">
+      <h1>Marika Abe Lamour</h1>
+      <p>
+        Full stack web developper with a year experience creating Individual
+        interview management software. I enjoy strolling around when I have time
+        to refresh myself.
+      </p>
+      <ul className="skill-list">
+        {skills.map((skill, index) => (
+          <Skill
+            key={index}
+            name={skill.name}
+            emoji={skill.emoji}
+            backgroundColor={skill.backgroundColor}
+          />
+        ))}
+      </ul>
+    </main>
+  );
+}
+
+function Skill({ name, emoji, backgroundColor }) {
+  return (
+    <li className="skill" style={{ backgroundColor: backgroundColor }}>
+      {name}
+      {emoji}
+    </li>
+  );
+}
